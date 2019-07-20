8232e244f8ca8decbdbf8240c583554052b1ec95
Opeious <tapanranjan@gmail.com>
2019-07-20 14:02:28 +0530

    Integrated battle ui

---
+++ Assets/Scripts/Singletons/GameSaveManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public interface ISaveData
+{
+    Dictionary<string, object> OnSaveData(Dictionary<string, object> saveDict);
+
+    void OnLoadData(Dictionary<string, object> saveDict);
+}
+
+public class GameSaveManager : MonoBehaviour
+{
+    public static bool loadedOnce = false;
+
+    static List<ISaveData> allSaveManagers = new List<ISaveData>();
+
+    public static GameSaveManager Instance = null;
+
+    bool saveStateDirty = false;
+
+    void Awake()
+    {
+        Instance = this;
+        GameSaveManager.saveFilePath = Application.persistentDataPath +  "/AutoSave.sav";
+    }
+
+    void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    void Start()
+    {
+        //GameEvents.PeriodicUpdate5s += SetSaveDirty;
+    }
+
+    public void SetSaveDirty()
+    {
+        saveStateDirty = true;
+    }
+
+    static string saveFilePath;
+
+    void Update()
+    {
+        if (saveStateDirty)
+        {
+            if(!loadedOnce) {
+                LoadDataToManagers();
+            }
+            saveStateDirty = false;
+
+            Dictionary<string, object> toWriteToFile = new Dictionary<string, object>();
+            foreach (var singleSaveManager in allSaveManagers)
+            {
+                Dictionary<string, object> dictToSave = new Dictionary<string, object>();
+                dictToSave = singleSaveManager.OnSaveData(dictToSave);
+                toWriteToFile.Add(singleSaveManager.GetType() + "", dictToSave);
+            }
+            string dataToFile = MiniJSON.Json.Serialize(toWriteToFile);
+
+            FileInfo file = new FileInfo(saveFilePath);
+            file.Directory.Create();
+            File.WriteAllText(file.FullName, dataToFile);
+        }
+    }
+
+    public static void LoadDataToManagers()
+    {
+        loadedOnce = true;
+        if(File.Exists(saveFilePath)) {
+            string dataAsJson = File.ReadAllText(saveFilePath); 
+            // var parsedJSONData = SimpleJSON.JSON.Parse(dataAsJson);
+            var parsedJSONData = MiniJSON.Json.Deserialize(dataAsJson);
+            Dictionary<string, object> dictToLoad = (Dictionary<string, object>) (parsedJSONData);
+            foreach (var singleSaveManager in allSaveManagers) {
+                if(dictToLoad.ContainsKey(singleSaveManager.GetType() + "")) {
+                    singleSaveManager.OnLoadData((Dictionary<string, object>)dictToLoad[singleSaveManager.GetType() + ""]);
+                }
+            }
+        }
+    }
+
+    public static void AddSavableManager(ISaveData savableManager)
+    {
+        allSaveManagers.Add(savableManager);
+    }
+}
---
+++ Assets/Scripts/Singletons/TeamSaveManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class TeamSaveManager : MonoBehaviour, ISaveData
+{
+    public CharacterClassTypes[] teamComp;
+
+    private void Awake() {
+        teamComp = new CharacterClassTypes[3];
+        GameSaveManager.AddSavableManager(this);
+    }
+
+    private void Start() {
+        GameSaveManager.Instance.SetSaveDirty();
+    }
+
+    public Dictionary<string, object> OnSaveData(Dictionary<string, object> saveDict) {
+        Dictionary<string, object> test = new Dictionary<string, object>();
+        int iterator = 1;
+        foreach (var singleComp in teamComp) {
+            test.Add(iterator++ + "", singleComp);
+        }
+        saveDict.Add("tcomp", test);
+        return saveDict;
+    }
+    
+    public void OnLoadData(Dictionary<string, object> saveDict) {
+        Debug.Log("p");
+        var test = saveDict["tcomp"] as Dictionary<string, object>;
+
+        int iterator = 0;
+        foreach(var kvp in test) {
+            if(iterator < teamComp.Length) {
+                teamComp[iterator++] = (CharacterClassTypes) Enum.Parse(typeof(CharacterClassTypes), kvp.Value + "");
+            }
+        }
+
+        Debug.Log("p");
+    }
+}
