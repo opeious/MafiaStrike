7e98832af3585b3bb5b2c3c6084aa0c6ac71cb7a
Opeious <tapanranjan@gmail.com>
2019-07-23 01:52:19 +0530

    stuff

--- Assets/Scripts/Singletons/TimeScaleManager.cs
+++ Assets/Scripts/Singletons/TimeScaleManager.cs
@@ -9,6 +9,8 @@ public class TimeScaleManager : MonoBehaviour
     public float slowDownFactor = 0.0005f;
     public float slowDownLength = 8f;
 
+    public bool returnBack;
+
     private void Awake() {
         Instance = this;
     }
@@ -18,12 +20,19 @@ public class TimeScaleManager : MonoBehaviour
     }
 
     void Update() {
-        Time.timeScale += (1f / slowDownLength) * Time.unscaledDeltaTime;
-        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        if(returnBack) {
+            Time.timeScale += (1f / slowDownLength) * Time.unscaledDeltaTime;
+            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        }
     }
 
     public void EnterSloMo() {
+        returnBack = false;
         Time.timeScale = slowDownFactor;
         Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+
+    public void ExitSloMo() {
+        returnBack = true;
     }
 }
