0a2119492ced7d11d8017909e3b04fc86109c819
Maitrayadhrit <zeldafan_mimo@yahoo.com>
2019-07-23 22:28:25 +0530

    Update

---
+++ Assets/Shaders/CustomToonLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomToonLight : MonoBehaviour
+{
+    private Light light = null;
+    private void OnEnable()
+{
+light = this.GetComponent<Light>();
+}
+
+private void Update()
+{
+Shader.SetGlobalVector("Toon", -this.transform.forward);
+}
+
+}
